4cb6bf7fa62f2ef3d1345e6af9fbc0662ea26715
Michael <109005544+NoMad100@users.noreply.github.com>
2023-05-23 12:27:40 -0700

    merging changes

--- src/main.js
+++ src/main.js
@@ -9,16 +9,5 @@ let config = {
     scene: [test],
     title: "Right Way"
 }
-let config = {
-    scale: {
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        mode: Phaser.Scale.FIT,
-        width: 800,
-        height: 600,
-    },
-    backgroundColor: 0xA00000,
-    scene: Menu,
-    title: "Right Way",
-}
 
 let game = new Phaser.Game(config);
